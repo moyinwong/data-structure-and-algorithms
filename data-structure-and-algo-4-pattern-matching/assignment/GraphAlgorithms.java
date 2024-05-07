f50573053748b0cd948853ad8caa3488baafc23b
Kelvin Wong <36435556+moyinwong@users.noreply.github.com>
2024-05-07 14:54:43 +0800

    module 14 prim assignment

--- data-structure-and-algo-4-pattern-matching/assignment/GraphAlgorithms.java
+++ data-structure-and-algo-4-pattern-matching/assignment/GraphAlgorithms.java
@@ -1,5 +1,6 @@
 package assignment;
 
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -30,24 +31,115 @@ public class GraphAlgorithms {
 
         Set<Edge<Character>> edges = new HashSet<>();
         edges.add(new Edge<>(a, b, 1));
-        edges.add(new Edge<>(a, e, 2));
-        edges.add(new Edge<>(a, d, 7));
-        edges.add(new Edge<>(b, c, 8));
-        edges.add(new Edge<>(b, f, 7));
-        edges.add(new Edge<>(c, g, 5));
-        edges.add(new Edge<>(c, a, 2));
-        edges.add(new Edge<>(g, f, 4));
-        edges.add(new Edge<>(g, h, 2));
-        edges.add(new Edge<>(g, e, 1));
-        edges.add(new Edge<>(f, g, 9));
-        edges.add(new Edge<>(f, h, 9));
-        edges.add(new Edge<>(d, f, 6));
-        edges.add(new Edge<>(d, b, 7));
-        edges.add(new Edge<>(d, h, 8));
-        edges.add(new Edge<>(h, g, 2));
+        edges.add(new Edge<>(b, a, 1));
+        edges.add(new Edge<>(a, c, 5));
+        edges.add(new Edge<>(c, a, 5));
+        edges.add(new Edge<>(b, c, 7));
+        edges.add(new Edge<>(c, b, 7));
+        edges.add(new Edge<>(b, g, 1));
+        edges.add(new Edge<>(g, b, 1));
+        edges.add(new Edge<>(b, d, 9));
+        edges.add(new Edge<>(d, b, 9));
+        edges.add(new Edge<>(c, g, 13));
+        edges.add(new Edge<>(g, c, 13));
+        edges.add(new Edge<>(d, f, 4));
+        edges.add(new Edge<>(f, d, 4));
+        edges.add(new Edge<>(f, g, 2));
+        edges.add(new Edge<>(g, f, 2));
+        edges.add(new Edge<>(f, h, 0));
+        edges.add(new Edge<>(h, f, 0));
+        edges.add(new Edge<>(h, g, 3));
+        edges.add(new Edge<>(g, h, 3));
+        edges.add(new Edge<>(g, e, 10));
+        edges.add(new Edge<>(e, g, 10));
+
         Graph<Character> graph = new Graph<>(vertices, edges);
 
-        System.out.println(bfs(a, graph));
+        var set = prims(b, graph);
+        if (set == null) {
+            return;
+        }
+        set.forEach(ele -> {
+            System.out.printf("From: %s, to: %s, edge weight: %d\n", ele.getU().getData().toString(), ele.getV().getData().toString(), ele.getWeight());
+        });
+    }
+
+    /**
+     * Runs Prim's algorithm on the given graph and returns the Minimum
+     * Spanning Tree (MST) in the form of a set of Edges. If the graph is
+     * disconnected and therefore no valid MST exists, return null.
+     * <p>
+     * You may assume that the passed in graph is undirected. In this framework,
+     * this means that if (u, v, 3) is in the graph, then the opposite edge
+     * (v, u, 3) will also be in the graph, though as a separate Edge object.
+     * <p>
+     * The returned set of edges should form an undirected graph. This means
+     * that every time you add an edge to your return set, you should add the
+     * reverse edge to the set as well. This is for testing purposes. This
+     * reverse edge does not need to be the one from the graph itself; you can
+     * just make a new edge object representing the reverse edge.
+     * <p>
+     * You may assume that there will only be one valid MST that can be formed.
+     * <p>
+     * You should NOT allow self-loops or parallel edges in the MST.
+     * <p>
+     * You may import/use java.util.PriorityQueue, java.util.Set, and any
+     * class that implements the aforementioned interface.
+     * <p>
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     * <p>
+     * The only instance of java.util.Map that you may use is the adjacency
+     * list from graph. DO NOT create new instances of Map for this method
+     * (storing the adjacency list in a variable is fine).
+     * <p>
+     * You may assume that the passed in start vertex and graph will not be null.
+     * You may assume that the start vertex exists in the graph.
+     *
+     * @param <T>   The generic typing of the data.
+     * @param start The vertex to begin Prims on.
+     * @param graph The graph we are applying Prims to.
+     * @return The MST of the graph or null if there is no valid MST.
+     */
+    public static <T> Set<Edge<T>> prims(Vertex<T> start, Graph<T> graph) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        Set<Vertex<T>> visited = new HashSet<>();
+        Set<Edge<T>> mst = new HashSet<>();
+        Queue<Edge<T>> queue = new PriorityQueue<>();
+        Map<Vertex<T>, List<VertexDistance<T>>> adjList = graph.getAdjList();
+        int n = graph.getVertices().size();
+
+        addNeighbourEdgesToQueue(start, adjList, queue, visited);
+        visited.add(start);
+
+        while (visited.size() < n && mst.size() < (2 * (n - 1)) && !queue.isEmpty()) {
+            Edge<T> edge = queue.poll();
+            Vertex<T> from = edge.getU();
+            Vertex<T> to = edge.getV();
+
+            if (!visited.contains(to)) {
+                visited.add(to);
+                mst.add(edge);
+                mst.add(new Edge<>(to, from, edge.getWeight()));
+
+                addNeighbourEdgesToQueue(to, adjList, queue, visited);
+            }
+        }
+
+
+        if (mst.size() != 2 * (n - 1) || visited.size() < n) {
+            return null;
+        }
+
+        return mst;
+    }
+
+    private static <T> void addNeighbourEdgesToQueue(Vertex<T> source, Map<Vertex<T>, List<VertexDistance<T>>> adjList, Queue<Edge<T>> queue, Set<Vertex<T>> visited) {
+        for (VertexDistance<T> v : adjList.get(source)) {
+            if (!visited.contains(v.getVertex())) {
+                queue.offer(new Edge<>(source, v.getVertex(), v.getDistance()));
+            }
+        }
     }
 
     /**
--- data-structure-and-algo-4-pattern-matching/graph.md
+++ data-structure-and-algo-4-pattern-matching/graph.md
@@ -94,11 +94,14 @@ public static <T> Map<Vertex<T>, Integer> dijstra(Vertex<T> start, Graph<T> grap
 ## Prim's Algorithm
 
 It's a **greedy algorithm** for finding **Minimum Spanning Tree (MST)** by considering the shortest edges the connect
-all the vertices. The algorithm makes use of 2 auxiliary data structures:
+all the vertices. The algorithm makes use of 3 auxiliary data structures:
 
 1. Priority Queue
 2. visited set
 3. MST edge set
+
+Graph cut, takes a subset of vertices and all edges connecting them. Any MST must include the minimum cost edge over any
+graph cut
 
 ```java
  public static <T> Set<VertexDistance<T>> prim(Vertex<T> start, Graph<T> graph) {
@@ -130,6 +133,56 @@ all the vertices. The algorithm makes use of 2 auxiliary data structures:
     return mstEdges;
 }
 ```
+
+### Time complexity
+
+- `E log E`
+    - the priority queue could add entries `E` times. Each add is `log E` for binary heap
+    - and it could also remove entries `E` times. Each removal is `log E` for binary heap
+
+## Kruskal's Algorithm
+
+Find the MST or Minimum Spanning Forest (MSF) for an undirected, connected and weighted graph. It utilise three
+auxiliary data structures:
+
+1. Priority Queue
+2. Disjoint set
+3. MST edge set
+
+The algorithm:
+
+- adding all edges into the priority queue
+- While the priority queue is not empty and MST edge set size is not `n - 1` (no. of vertices in graph)
+    - dequeue an edge from the queue
+    - if the `from` and `to` vertex doesn't form a cycle, add the edge to the MST
+
+Disjoint set is used to detect if cycle would be formed by:
+
+- detecting if two vertices are connected (if they are in the same component)
+- merge/union two connected components efficiently so that they are in the same component
+
+It has two operations:
+
+- `find(s)`, find the root/representative of a given vertex. Follow the outgoing edge to ancestors until reaching the
+  root. Will need efficient access to where is located in the disjoint set forest. This is accomplished by using a
+  HashMap
+  from vertex objects in the graph to node objects in the disjoint set forest.
+- `union(u, v)`, merge the sets that `u` and `v` belong to. First need to find the roots of `find(u)` and
+  then `find(v)`. Make either `root(u)` a child of `root(v)` or vice versa. This ensure the representative of the
+  component is unique
+
+Two optimizations can be made for the disjoint set operations
+
+1. **Path compression** for `find(s)`, making all nodes in a subtree point to same root on the recursive call to find
+   root for a particular node
+2. **Union by rank** for `union(u, v)`, serves as upper bound for height with path compression. Rank can only be higher
+   than the actual height of a vertex with path compression:
+    - Each node's initial rank is set to 0. Once we compute and `root(u)` and `root(v)`, we check
+      if `rank(root(u))` > `rank(root(v))`. If so, `root(v)` becomes a child of `root(u)`; the rank doesn't change.
+    - If `rank(root(v))` > `rank(root(u))`, then `root(u)` becomes the child of `root(v)`, and the rank doesn't change.
+      Otherwise, the ranks are equal,
+      so we can make either the child. For example, if we make `root(v)` the child of `root(u)`, then we update the rank
+      like so: .`rank(root(u)) = rank(root(u)) + 1`
 
 ### Time complexity
 
